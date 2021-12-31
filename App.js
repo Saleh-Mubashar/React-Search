6914963f3cdc3e6237d1df38dc20756f7d8b23f9
RehmatClick <34207110+RehmatClick@users.noreply.github.com>
2021-12-31 13:22:48 +0300

    search

---
+++ App.js
@@ -0,0 +1,31 @@
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "./Components/List";
+import "./App.css";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
+  return (
+    <div className="main">
+      <h1>React Search</h1>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          onChange={inputHandler}
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+      <List input={inputText} />
+    </div>
+  );
+}
+
+export default App;
