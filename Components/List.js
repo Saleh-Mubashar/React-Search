1a77d9cf41911bd9bdd8bde11f1541a9b555596f
Saleh-Mubashar <88970635+Saleh-Mubashar@users.noreply.github.com>
2021-12-31 19:11:24 +0300

    Update and rename Gallery.js to List.js

---
+++ Components/List.js
@@ -0,0 +1,21 @@
+import { React, useState } from 'react'
+import data from "./ListData.json"
+
+function List(props) {
+    const filteredData = data.filter((el) => {
+        if (props.input === '') {
+            return el;
+        } else {
+            return el.text.toLowerCase().includes(props.input)
+        }
+    })
+    return (
+        <ul>
+            {filteredData.map((item) => (
+                <li key={item.id}>{item.text}</li>
+            ))}
+        </ul>
+    )
+}
+
+export default List
